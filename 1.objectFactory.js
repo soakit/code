2e846389f8c6783409c54e3581c6147cb098403a
soakit <455288262@qq.com>
2020-06-09 23:31:47 +0800

    update

--- 1.objectFactory.js
+++ 1.objectFactory.js
@@ -1,8 +1,9 @@
 /**
  * 实现:new
- * 1.创建一个空对象obj = {}
- * 2.设置空对象的原型链->对象的__proto__指向构造函数的prototype
- * 3.将构造函数的this指向obj，并执行
+ * objectFactory(Person, 'zhangsan', 24)
+ * 1.创建一个空对象obj
+ * 2.设置obj的原型链(对象的__proto__)指向构造函数的prototype
+ * 3.将构造函数的this指向obj，并执行(Person.apply(obj))
  * 4.判断执行的返回值类型，是对象则返回，否则返回obj
  * (js中的构造函数，是不需要有返回值的，所以默认返回的是新创建的空对象obj)
  */
