fd2c90b387549c48e209480644595e3449d09871
soakit <455288262@qq.com>
2020-06-14 22:29:27 +0800

    update content

--- 3.functionCallApply.js
+++ 3.functionCallApply.js
@@ -1,29 +1,30 @@
 // 思路：将要改变this指向的方法挂到目标this上执行并返回
-Function.prototype.mycall = function (context) {
+Function.prototype.mycall = function () {
     if (typeof this !== 'function') {
         throw new TypeError('not funciton')
     }
-    context = context || window
-    context.fn = this
-    let arg = [...arguments].slice(1)
-    let result = context.fn(...arg)
-    delete context.fn
-    return result
+    var [context, ...args] = arguments
+	var ctx = context || window
+	ctx.fn = this
+	var result = ctx.fn(...args)
+	delete ctx.fn
+	return result
 }
 
 // 思路：将要改变this指向的方法挂到目标this上执行并返回
-Function.prototype.myapply = function (context) {
+Function.prototype.myapply = function () {
     if (typeof this !== 'function') {
         throw new TypeError('not funciton')
     }
-    context = context || window
-    context.fn = this
-    let result
-    if (arguments[1]) {
-        result = context.fn(...arguments[1])
-    } else {
-        result = context.fn()
-    }
-    delete context.fn
-    return result
+    var [context, ...args] = arguments
+	var ctx = context || window
+	ctx.fn = this
+	var result
+	if (args.length) {
+		result = ctx.fn(...args)
+	} else {
+		result = ctx.fn()
+	}
+	delete ctx.fn
+	return result
 }
