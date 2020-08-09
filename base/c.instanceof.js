33cf46af57afa563c131f9ecf724fd2146cef9c4
soakit <liuchang2024@gmail.com>
2020-08-09 21:52:13 +0800

    更新instanceof

--- base/c.instanceof.js
+++ base/c.instanceof.js
@@ -1,22 +1,41 @@
-function myInstanceof(l, r) {
-    let right = r.prototype
-    let left = l.__proto__
-    while (true) {
-        if (left === null || right === undefined) {
+function myInstaceOf(instance, Ctor) {
+    if (typeof Ctor !== 'function') {
+        throw new Error('Ctor应该是一个Function')
+    }
+    // 基础类型则直接返回false
+    if (!instance || (typeof instance !== 'object' && typeof instance !== 'function')) {
+        return false
+    }
+    const CtorPrototype = Ctor.prototype
+    if (!CtorPrototype) {
+        // 考虑Ctor箭头函数
+        return false
+    }
+    while (instance) {
+        // 方式一
+        // var instancePrototype = instance.__proto__
+        // 方式二、getProtypeOf是Object对象自带的一个方法，能够拿到参数的原型对象
+        const instancePrototype = Object.getPrototypeOf(instance);
+        if (!instancePrototype) {
             return false
         }
-        if (left === right) {
+        if (instancePrototype === CtorPrototype) {
             return true
         }
-        left = left.__proto__
+        instance = instancePrototype
     }
 }
 
-// test
-var Animal = function(name) {
-    this.name = name
-}
+// 测试
+var Person = function () { }
+var p1 = new Person()
+console.log(myInstaceOf(p1, Person)) // true
 
-var dog = new Animal('dog')
+var str1 = 'hello world'
+console.log(myInstaceOf(str1, String)) // false
 
-console.log('myInstanceof(dog, Animal) => ', myInstanceof(dog, Animal))
+var str2 = new String('hello world')
+console.log(myInstaceOf(str2, String)) // true
+
+var arrowFn = () => {}
+console.log(myInstaceOf(arrowFn, Function)) // true
