feff6ff23ade3d46b4533c1b542153357cd90209
soakit <liuchang2024@gmail.com>
2020-09-11 10:36:53 +0800

    更新题解

--- algorithm/02.list/0019.js
+++ algorithm/02.list/0019.js
@@ -19,13 +19,20 @@
  * @param {number} n
  * @return {ListNode}
  */
+/* 
+1. 使用哨兵节点，哨兵节点的下一节点为头节点
+2. p1，p2都指向哨兵，走的距离between
+3. 先只让p2往前走，当p1与p2的距离大于n时，让p1与p2一起往前走
+4. 当p2走到尽头，将p1.next指向p1.next.next
+5. 最后返回哨兵的下一节点
+*/
 var removeNthFromEnd = function(head, n) {
     const dummy = new ListNode(0, head)
     let p1 = dummy
     let p2 = dummy
     let between = 0
     while (p2) {
-      // 如果已经相隔n-1了，就同步一起走
+      // 如果已经相隔n了，就同步一起走
       // 否则只让p2走
       p2 = p2.next
       if (between > n) {
@@ -33,7 +40,6 @@ var removeNthFromEnd = function(head, n) {
       }
       between++;
     }
-    let ptr = p1.next
-    p1.next = ptr.next
+    p1.next = p1.next.next
     return dummy.next 
   };
