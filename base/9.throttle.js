09fa44c5022837ad317c3238a8508819bc463efe
soakit <liuchang2024@gmail.com>
2020-09-30 15:42:51 +0800

    优化代码

--- base/9.throttle.js
+++ base/9.throttle.js
@@ -1,20 +1,18 @@
-var throttle = function(fn, delay) {
-    var canRun = true
+var throttle = function (fn, delay) {
+  var canRun = true;
 
-    return function() {
-        var args = arguments
-        var context = this
-        if (!canRun) {
-            return
-        }
-        canRun = false
-        setTimeout(function() {
-            fn.apply(context, args)
-            canRun = true
-        }, delay || 500)
+  return function (...args) {
+    if (!canRun) {
+      return;
     }
-}
+    canRun = false;
+    setTimeout(() => {
+      fn.apply(this, args);
+      canRun = true;
+    }, delay || 500);
+  };
+};
 
-window.onresize = throttle(function() {
-    console.log(1)
-}, 500)
+window.onresize = throttle(function () {
+  console.log(1);
+}, 500);
--- base/e.debounce.js
+++ base/e.debounce.js
@@ -1,18 +1,17 @@
-var debounce = function(fn, delay) {
-    var timer
-    return function() {
-        var context = this
-        var args = arguments
-        clearTimeout(timer)
-        timer = setTimeout(function() {
-            fn.apply(context, args)
-        }, delay)
-    }
-}
+var debounce = function (fn, delay) {
+  var timer;
+  return function (...args) {
+    clearTimeout(timer);
+    timer = setTimeout(() => {
+      fn.apply(this, args);
+    }, delay);
+  };
+};
 
 // test
-document.getElementById('input').addEventListener('keyup', 
-    debounce(function(e) {
-        console.log(e.target.value)
-    }, 200)
-)
+document.getElementById("input").addEventListener(
+  "keyup",
+  debounce(function (e) {
+    console.log(e.target.value);
+  }, 200)
+);
