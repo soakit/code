7e27909c315da41d623c83739c552d33c775473e
soakit <liuchang2024@gmail.com>
2020-10-27 09:48:55 +0800

    增加题解

---
+++ algorithm/03.string/0301.js
@@ -0,0 +1,17 @@
+/* 
+    第344题：反转字符串
+    编写一个函数，其作用是将输入的字符串反转过来。输入字符串以字符数组 char[] 的形式给出。
+    不要给另外的数组分配额外的空间，你必须原地修改输入数组、使用 O(1) 的额外空间解决这一问题。 
+*/
+/**
+ * @param {character[]} s
+ * @return {void} Do not return anything, modify s in-place instead.
+ */
+var reverseString = function(s) {
+    for (let i=0, j=s.length-1; i<j; i++,j--) {
+      const t = s[i];
+      s[i] = s[j];
+      s[j] = t;
+    }
+    return s;
+  };
