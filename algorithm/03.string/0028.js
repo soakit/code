14c43907931b7d7ebf9e20712a676525ec28c54c
soakit <liuchang2024@gmail.com>
2020-10-28 09:49:06 +0800

    update

--- algorithm/03.string/0028.js
+++ algorithm/03.string/0028.js
@@ -1,3 +1,7 @@
+/* 
+  第28题：实现 strStr()
+  给定一个 haystack 字符串和一个 needle 字符串，在 haystack 字符串中找出 needle 字符串出现的第一个位置 (从0开始)。如果不存在，则返回 -1。 
+*/
 /**
  * @param {string} haystack
  * @param {string} needle
