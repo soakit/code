58c032f14ecb29aa0e2e7805c6b82b099dea868f
soakit <liuchang2024@gmail.com>
2020-10-29 16:55:28 +0800

    增加题解

---
+++ algorithm/04.dp/0070.js
@@ -0,0 +1,25 @@
+/* 
+    第70题：爬楼梯
+    假设你正在爬楼梯。需要 n 阶你才能到达楼顶。每次你可以爬 1 或 2 个台阶。你有多少种不同的方法可以爬到楼顶呢？ **注意：**给定 n 是一个正整数。 
+*/
+/**
+ * @param {number} n
+ * @return {number}
+ */
+var climbStairs = function (n) {
+  if (n <= 2) {
+    return n;
+  }
+  // 一步一走有2种解法
+  // 两步一走有1种解法
+  let oneStepWay = 2;
+  let twoStepWay = 1;
+  let allWays = 0;
+
+  for (let i = 2; i < n; i++) {
+    allWays = oneStepWay + twoStepWay;
+    twoStepWay = oneStepWay;
+    oneStepWay = allWays;
+  }
+  return allWays;
+};
