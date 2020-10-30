2e74b9a70afdf870e1dfdb36906ad5f520890f92
soakit <liuchang2024@gmail.com>
2020-10-30 15:10:26 +0800

    更新题解

--- algorithm/04.dp/0070.js
+++ algorithm/04.dp/0070.js
@@ -23,3 +23,17 @@ var climbStairs = function (n) {
   }
   return allWays;
 };
+
+// dp公式
+var climbStairs = function (n) {
+  if (n <= 2) {
+    return n;
+  }
+  var dp = [];
+  dp[0] = 1;
+  dp[1] = 2;
+  for (let i = 2; i < n; i++) {
+    dp[i] = dp[i - 1] + dp[i - 2];
+  }
+  return dp[n - 1];
+};
