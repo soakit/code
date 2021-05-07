d2327b60c66a45463a4e7d6aa43cd8a7728bc53c
soakit <liuchang2024@gmail.com>
2021-05-07 16:22:07 +0800

    chore: update 06

--- ts-exercises/06.ambient_modules/declarations/date-wizard/index.d.ts
+++ ts-exercises/06.ambient_modules/declarations/date-wizard/index.d.ts
@@ -2,6 +2,10 @@
 // This enables module augmentation mode.
 import "../../modules/date-wizard";
 declare module "date-wizard" {
+  // function方式
+  function dateWizard(date: string, format: string) : string;
+  
+  // const方式
   const pad: (ident: number) => string;
 
   interface DateDetails {
