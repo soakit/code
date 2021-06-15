84375f7d5f90ccc37404d4d21136bf7ffb55eb36
soakit <liuchang2024@gmail.com>
2021-06-15 16:12:06 +0800

    feat: add solidity

---
+++ blockchain/sample.sol
@@ -0,0 +1,43 @@
+// 版本号
+pragma solidity >=0.7.0 <0.9.0;
+
+/**
+ * @title Storage
+ * @dev Store & retrieve value in a variable
+ */
+contract Storage {
+    uint256 number;
+    address owner;
+
+    struct Student {
+        uint256 id;
+        string name;
+        uint256 age;
+    }
+
+    Student stu;
+
+    constructor() {
+        // msg.sender 合约部署者
+        owner = msg.sender;
+        stu = Student(0, "zhangsan", 18);
+    }
+
+    /**
+     * @dev Store value in variable
+     * @param num value to store
+     */
+    function store(uint256 num) public {
+        number = num;
+    }
+
+    /**
+     * @dev Return value
+     * @return value of 'number'
+     */
+    // view函数 只读取变量，不写入
+    // returns 返回值类型
+    function retrieve() public view returns (uint256) {
+        return number;
+    }
+}
---
+++ blockchain/solidity.md
@@ -0,0 +1,55 @@
+# solidity
+
+## 1. 格式
+
+sample.sol
+
+## 2.数据类型
+
+- 布尔值：true false
+  - 逻辑运算符：&& || !
+- 整型
+  - uint：无符号整型，智能表示正整数
+  - int：和 js 里的 number 类似
+- address
+  地址，0x...一共 42 个字符，0x 表示 16 进制，共 160bit
+  - 合约里的全局变量：msg.sender
+  - address 类型的方法
+    - blance 查看余额
+    - transfer 转账
+- string
+  - string name = "zhangsan";
+- 数组
+
+  ```sol
+    uint [5] arr = [1,2,3,4,5];
+    arr[1] = 1;
+    arr.push(6);
+  ```
+
+- map
+
+  ```sol
+  mapping(address => uint) users;
+  users["address1"] = 100;
+  users["address2"] = 10;
+  ```
+
+- struct: 结构体
+
+  ```sol
+    struct Student {
+      uint id;
+      string name;
+      uint age;
+    }
+    zhangsan = Student(0, "zhangsan", 18)
+  ```
+
+- enum
+
+  ```sol
+    enum sex {
+      male, female
+    }
+  ```
